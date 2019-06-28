e5de22a3d9efbec2faa7fce1739a9cd9c6c9aadd
lf <389175923@qq.com>
2019-06-28 15:19:54 +0800

    confused ...... 2019年6月28日15:19:46

---
+++ src/test/java/cn/snowpic/day_6_28/GenericTest.java
@@ -0,0 +1,34 @@
+package cn.snowpic.day_6_28;
+
+import org.junit.Test;
+
+/**
+ * @className GenericTest
+ * @description generic test
+ * @author lf
+ * @time 2019/6/28 10:54
+ **/
+
+public class GenericTest {
+
+    /**
+     * test1
+     * @author lf
+     * @time 2019/6/28 13:20
+     */
+    @Test
+    public void test1() {
+        System.out.println("What a wonderful day today.....");
+    }
+
+    /**
+     * test2 another test ....
+     * @author lf
+     * @time 2019/6/28 15:16
+     */
+    @Test
+    public void test2() {
+        System.out.println("I am confused about something," +
+                "but I can't resolve it");
+    }
+}
