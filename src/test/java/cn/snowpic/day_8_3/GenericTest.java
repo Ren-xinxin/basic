eb3a7ace0f678a568c77ca825bdb9e1519bd37c3
lf <389175923@qq.com>
2019-08-04 18:50:13 +0800

    今日份的代码编写

---
+++ src/test/java/cn/snowpic/day_8_3/GenericTest.java
@@ -0,0 +1,40 @@
+package cn.snowpic.day_8_3;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @className GenericTest
+ * @description
+ * @author lf
+ * @time 2019/8/3 23:03
+ **/
+
+public class GenericTest {
+
+    /**
+     * nio
+     * @author lf
+     * @time 2019/8/3 23:03
+     */
+    @Test
+    public void test1() throws IOException {
+        File file = new File("C:/Users/Administrator/Desktop/test.txt");
+        FileInputStream fis = new FileInputStream(file);
+        FileChannel fc = fis.getChannel();
+        ByteBuffer bb = ByteBuffer.allocate(100);
+        int bytes = fc.read(bb);
+        byte[] dest = new byte[bytes];
+        System.arraycopy(bb.array(), 0, dest, 0, bytes);
+        String s = new String(dest, Charset.forName("GB2312"));
+        System.out.println(s);
+    }
+}
