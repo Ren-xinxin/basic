04ae8f7415291ce9cb3463b611bdd1e32e9d7c4c
lf <389175923@qq.com>
2019-09-22 13:57:18 +0800

    iterator design pattern...

--- src/test/java/cn/snowpic/day_9_22/GenericTest.java
+++ src/test/java/cn/snowpic/day_9_22/GenericTest.java
@@ -3,7 +3,10 @@ package cn.snowpic.day_9_22;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 
 /**
  * @className GenericTest
@@ -29,6 +32,76 @@ public class GenericTest {
         watched.addWatcher(evildoer);
 
         watched.notifyAllWatchers();
+    }
+
+    /**
+     * iterator design pattern
+     * @author lf
+     * @time 2019/9/22 12:50
+     */
+    @Test
+    public void test2() {
+        List<Product> products = new ArrayList<Product>() {{
+            add(new Product("hat"));
+            add(new Product("paper"));
+            add(new Product("pen"));
+            add(new Product("mouse"));
+        }};
+
+        ProductIterator iterator = new ProductIterator(products);
+
+        for (Product product : iterator) {
+            System.out.println(product);
+        }
+    }
+}
+
+class Product {
+    private String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "\"name\":\"" + name + "\"" +
+                "}";
+    }
+}
+
+class ProductIterator implements Iterator<Product>,Iterable<Product> {
+
+    private List<Product> products;
+
+    private int index = 0;
+
+    public ProductIterator(List<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < this.products.size();
+    }
+
+    @Override
+    public Product next() {
+        return this.products.get(index++);
+    }
+
+    @Override
+    public Iterator<Product> iterator() {
+        return this;
     }
 }
 
