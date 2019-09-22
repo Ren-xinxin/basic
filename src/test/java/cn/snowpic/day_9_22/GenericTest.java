ab1a51f785b711211a74f0be801d3df6526eb88e
lf <389175923@qq.com>
2019-09-22 15:39:57 +0800

    memento design pattern...

--- src/test/java/cn/snowpic/day_9_22/GenericTest.java
+++ src/test/java/cn/snowpic/day_9_22/GenericTest.java
@@ -68,6 +68,109 @@ public class GenericTest {
         change.execute();
         off.execute();
     }
+
+    /**
+     * memento design pattern
+     * @author lf
+     * @time 2019/9/22 15:29
+     */
+    @Test
+    public void test5() {
+        Person tom = new Person("Tom", 11, 170.1, 63.0);
+        System.out.println("tom = " + tom);
+        Caretaker caretaker = new Caretaker(tom.createMemento());
+        tom.setAge(18);
+        tom.setHeight(18);
+        System.out.println("tom = " + tom);
+
+        tom.restore(caretaker.getMemento());
+        System.out.println("tom = " + tom);
+
+    }
+
+}
+
+class Caretaker {
+    private Person memento;
+
+    public Caretaker(Person memento) {
+        this.memento = memento;
+    }
+
+    public Person getMemento() {
+        return memento;
+    }
+
+    public void setMemento(Person memento) {
+        this.memento = memento;
+    }
+}
+
+class Person {
+    private String name;
+    private int age;
+    private double height;
+    private double weight;
+
+    public Person(String name, int age, double height, double weight) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public Person createMemento() {
+        return new Person(this.name, this.age, this.height, this.weight);
+    }
+
+    public void restore(Person person) {
+        this.name = person.name;
+        this.age = person.age;
+        this.height = person.height;
+        this.weight = person.weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "\"name\":\"" + name + "\"" +
+                ",\"age\":" + age +
+                ",\"height\":" + height +
+                ",\"weight\":" + weight +
+                "}";
+    }
 }
 
 class CommandChannelChange implements Command {
