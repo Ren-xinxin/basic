e7f147033c0bbe5673a559484e724d16a093a8ba
lf <389175923@qq.com>
2019-09-23 20:40:07 +0800

    bridge design pattern...

---
+++ src/test/java/cn/snowpic/day_9_23/GenericTest.java
@@ -0,0 +1,57 @@
+package cn.snowpic.day_9_23;
+
+import org.junit.Test;
+
+/**
+ * @className GenericTest
+ * @description
+ * @author lf
+ * @time 2019/9/23 20:33
+ **/
+
+public class GenericTest {
+
+    /**
+     * bridge pattern
+     * @author lf
+     * @time 2019/9/23 20:34
+     */
+    @Test
+    public void test1(){
+        MyCountry country0 = new MyCountry(new GreenBridge());
+        MyCountry country1 = new MyCountry(new YellowBridge());
+
+        country0.ShowPower();
+        country1.ShowPower();
+    }
+}
+
+class MyCountry{
+    private Bridge bridge;
+
+    public MyCountry(Bridge bridge) {
+        this.bridge = bridge;
+    }
+
+    void ShowPower(){
+        bridge.showColor();
+    }
+}
+
+interface Bridge{
+    void showColor();
+}
+
+class GreenBridge implements Bridge{
+    @Override
+    public void showColor() {
+        System.out.println("Green bridge");
+    }
+}
+
+class YellowBridge implements Bridge{
+    @Override
+    public void showColor() {
+        System.out.println("Yellow bridge");
+    }
+}
