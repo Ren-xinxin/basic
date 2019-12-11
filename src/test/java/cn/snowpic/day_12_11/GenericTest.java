1064a41565906fa29ab74f7a8624e04f4bcfbea2
lf <389175923@qq.com>
2019-12-11 22:36:26 +0800

    quick sort algorithm with one guard...

--- src/test/java/cn/snowpic/day_12_11/GenericTest.java
+++ src/test/java/cn/snowpic/day_12_11/GenericTest.java
@@ -7,6 +7,7 @@ package cn.snowpic.day_12_11;
 import org.junit.Test;
 
 import java.text.DecimalFormat;
+import java.util.Arrays;
 
 /**
  * @className GenericTest
@@ -40,5 +41,73 @@ public class GenericTest {
         double v = (Math.pow(phi, n) - Math.pow(1 - phi, n)) / Math.sqrt(5);
         DecimalFormat decimalFormat = new DecimalFormat("0");
         return decimalFormat.format(v);
+    }
+
+    /**
+     * test2
+     * @author lf
+     * @time 2019/12/11 21:15
+     */
+    @Test
+    public void test2() {
+        int[] arrays = {7, 4, 2, 8, 6, 9, 5, 8};
+        quickSort(arrays, null);
+
+        System.out.println("arrays = " + Arrays.toString(arrays));
+    }
+
+    /**
+     * quick sort
+     * @author lf
+     * @time 2019/12/11 21:51
+     * @param arrays arrays
+     * @param index index
+     */
+    public void quickSort(int[] arrays, Integer... index) {
+        if (index != null && index.length != 2) {
+            throw new IllegalArgumentException("Index must contain the left and the right index.");
+        }
+        int left = (index == null) ? 0 : index[0];
+        int right = (index == null) ? (arrays.length - 1) : index[1];
+        if (left < right) {
+            int partitionIndex = partition(arrays, left, right);
+            quickSort(arrays, left, partitionIndex - 1);
+            quickSort(arrays, partitionIndex + 1, right);
+        }
+    }
+
+    /**
+     * partition
+     * @author lf
+     * @time 2019/12/11 21:51
+     * @param arrays arrays
+     * @param left left
+     * @param right right
+     * @return int
+     */
+    private int partition(int[] arrays, int left, int right) {
+        int index = left + 1;
+        for (int i = index; i <= right; i++) {
+            if (arrays[i] < arrays[left]) {
+                swap(arrays, i, index);
+                index++;
+            }
+        }
+        swap(arrays, left, index - 1);
+        return index - 1;
+    }
+
+    /**
+     * swap
+     * @author lf
+     * @time 2019/12/11 21:51
+     * @param arrays arrays
+     * @param i i
+     * @param index index
+     */
+    private void swap(int[] arrays, int i, int index) {
+        int tem = arrays[i];
+        arrays[i] = arrays[index];
+        arrays[index] = tem;
     }
 }
