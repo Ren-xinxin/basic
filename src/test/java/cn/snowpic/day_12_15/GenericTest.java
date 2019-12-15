0d4747fb10e636705de821f0fedb71717f45ce27
lf <389175923@qq.com>
2019-12-15 22:58:16 +0800

    Yang Hui's delta algorithm..

--- src/test/java/cn/snowpic/day_12_15/GenericTest.java
+++ src/test/java/cn/snowpic/day_12_15/GenericTest.java
@@ -6,7 +6,9 @@ package cn.snowpic.day_12_15;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @className GenericTest
@@ -40,22 +42,72 @@ public class GenericTest {
         int i = 0;
         int j = 0;
         int k = 0;
-        while (k < result.length) {
-            if (i == arrays1.length) {
-                result[k++] = arrays2[j++];
-                continue;
-            }
-            if (j == arrays2.length) {
-                result[k++] = arrays1[i++];
-                continue;
-            }
+        while (i < arrays1.length && j < arrays2.length) {
             if (arrays1[i] < arrays2[j]) {
                 result[k++] = arrays1[i++];
             } else {
                 result[k++] = arrays2[j++];
             }
         }
+        if (i == arrays1.length) {
+            while (j < arrays2.length) {
+                result[k++] = arrays2[j++];
+            }
+        } else {
+            while (i < arrays1.length) {
+                result[k++] = arrays1[i++];
+            }
+        }
         System.out.println(i + " " + j + " " + k);
         return result;
     }
+
+    /**
+     * test2
+     * @author lf
+     * @time 2019/12/15 22:23
+     */
+    @Test
+    public void test2() {
+        int line = 10;
+        List<List<Integer>> delta = delta(line);
+        System.out.println("delta = " + delta);
+        /*String s = delta.toString();
+        String s1 = s.substring(1, s.length() - 2).replaceAll("\\s", "").replaceAll("\\[", "");
+        String[] split = s1.split("],");
+        for (int i = 0; i < split.length; i++) {
+            for (int j = 0; j < (line - 1 - i); j++) {
+                System.out.print(" ");
+            }
+            System.out.println(split[i].replaceAll(",", " "));
+        }*/
+    }
+
+    /**
+     * delta
+     * @author lf
+     * @time 2019/12/15 22:23
+     * @param line line
+     * @return @List<List<Integer>>
+     */
+    public List<List<Integer>> delta(int line) {
+        List<List<Integer>> result = new ArrayList<>(line);
+        if (line < 1) {
+            throw new IllegalArgumentException("line must greater than 0");
+        }
+        result.add(new ArrayList<Integer>() {{
+            add(1);
+        }});
+        for (int i = 2; i <= line; i++) {
+            List<Integer> child = new ArrayList<>();
+            List<Integer> lastRow = result.get(i - 2);
+            for (int j = 0; j < i; j++) {
+                int value = (j - 1 < 0 ? 0 : lastRow.get(j - 1)) + (j == lastRow.size() ? 0 : lastRow.get(j));
+                child.add(value);
+            }
+            result.add(child);
+        }
+        return result;
+    }
+
 }
