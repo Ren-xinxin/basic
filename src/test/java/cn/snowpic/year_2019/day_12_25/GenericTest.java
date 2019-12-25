333985b1961578f87682be1d9594efcdd0a8ff0d
lf <389175923@qq.com>
2019-12-25 20:03:29 +0800

    find spilt array's max sum algorithm...

--- src/test/java/cn/snowpic/year_2019/day_12_25/GenericTest.java
+++ src/test/java/cn/snowpic/year_2019/day_12_25/GenericTest.java
@@ -49,4 +49,38 @@ public class GenericTest {
         System.arraycopy(arrays, start, result, 0, result.length);
         return result;
     }
+
+    /**
+     * test2 find max sum that array([1,2,3,4]) spilt to a new array([1,2][3,4])
+     * @author lf
+     * @time 2019/12/25 19:59
+     */
+    @Test
+    public void test2() {
+        System.out.println(getMaximumSum(new int[]{1, 4, 3, 2, 5, 6}));
+    }
+
+    /**
+     * get maximum sum
+     * @author lf
+     * @time 2019/12/25 19:59
+     * @param arrays arrays
+     * @return int
+     */
+    private int getMaximumSum(int[] arrays) {
+        Arrays.sort(arrays);
+        if (arrays.length % 2 != 0) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 1; i < arrays.length; i++) {
+            if (arrays[i] - arrays[i - 1] != 1) {
+                throw new IllegalArgumentException();
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < arrays.length; i += 2) {
+            sum += arrays[i];
+        }
+        return sum;
+    }
 }
