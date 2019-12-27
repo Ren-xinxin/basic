121e078a87a76fd428fde21bfdcd5820b55e2ffd
lf <389175923@qq.com>
2019-12-27 21:21:20 +0800

    grow flowers problem algorithm..

--- src/test/java/cn/snowpic/year_2019/day_12_27/GenericTest.java
+++ src/test/java/cn/snowpic/year_2019/day_12_27/GenericTest.java
@@ -47,4 +47,37 @@ public class GenericTest {
         max = Math.max(max, count);
         return max;
     }
+
+    /**
+     * test2 of grow flowers
+     * @author lf
+     * @time 2019/12/27 21:17
+     */
+    @Test
+    public void test2() {
+        boolean flowers = growFlowers(new int[]{1, 0, 0, 0, 1}, 1);
+        System.out.println("flowers = " + flowers);
+    }
+
+    /**
+     * grow flowers
+     * @author lf
+     * @time 2019/12/27 21:01
+     * @param array array 0 presents empty 1 presents there is a flower.
+     *              You are not allowed to grow a flower near by another flower.
+     * @return boolean
+     */
+    private boolean growFlowers(int[] array, int flowers) {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if ((i == 0 || array[i - 1] == 0)
+                    && array[i] == 0
+                    && (i == array.length - 1 || array[i + 1] == 0)) {
+                if (++count >= flowers) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
