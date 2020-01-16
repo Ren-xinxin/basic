b4101f6bf285b180c603e693154103ea40cb20bf
Little Flower <snowpic@163.com>
2020-01-17 00:50:02 +0800

    maximum of container area algorithm...

---
+++ src/test/java/cn/snowpic/year_2020/day_01_17/GenericTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright(c) snowpic.cn 2019-2019.All rights reserved.
+ */
+
+package cn.snowpic.year_2020.day_01_17;
+
+import org.junit.Test;
+
+public class GenericTest {
+
+    /**
+     * test1
+     *
+     * @author lf
+     * @time 2020-01-17 0:39
+     */
+    @Test
+    public void test1() {
+        int[] heights = {1, 2, 5, 7, 8, 10, 12, 2, 34};
+        int area = maxContainerArea(heights);
+        System.out.println("area = " + area);
+    }
+
+    /**
+     * max container area
+     *
+     * @author lf
+     * @time 2020-01-17 0:39
+     * @param array array
+     * @return int
+     */
+    private int maxContainerArea(int[] array) {
+        int max = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                max = Math.max(max, Math.min(array[j], array[i]) * (j - i));
+            }
+        }
+        return max;
+    }
+}
