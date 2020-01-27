d86fb7b7b47adc136d1ffbc67980b6e49e56c320
Little Flower <snowpic@163.com>
2020-01-27 23:28:33 +0800

    unsuitable find sum algorithm...

--- src/test/java/cn/snowpic/year_2020/day_01_27/GenericTest.java
+++ src/test/java/cn/snowpic/year_2020/day_01_27/GenericTest.java
@@ -4,9 +4,13 @@
 
 package cn.snowpic.year_2020.day_01_27;
 
+import cn.snowpic.util.CommonUtils;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class GenericTest {
 
@@ -46,6 +50,37 @@ public class GenericTest {
                 }
                 result[1] = i + 1;
             }
+        }
+        return result;
+    }
+
+    @Test
+    public void test2() {
+        int[] array = {2, 3, 6, 7};
+        List<List<Integer>> sum = findSum(array, 7);
+        System.out.println("sum = " + sum);
+    }
+
+    /**
+     * find sum
+     *
+     * @author lf
+     * @time 2020-01-27 12:13
+     * @param array array
+     * @param target target
+     * @return @List<List<Integer>>
+     */
+    private List<List<Integer>> findSum(int[] array, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            List<List<Integer>> permutes = CommonUtils.permute(array, i + 1);
+            permutes.forEach(permute -> {
+                int sum = permute.stream().mapToInt(Integer::intValue).sum();
+                if (target == sum) {
+                    List<Integer> list = permute.stream().sorted().collect(Collectors.toList());
+                    result.add(list);
+                }
+            });
         }
         return result;
     }
