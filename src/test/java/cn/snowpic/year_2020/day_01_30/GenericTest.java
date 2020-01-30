d3a95d6f42dedac7c18941c466a9a06f1679064b
Little Flower <snowpic@163.com>
2020-01-31 00:25:40 +0800

    find common prefix algorithm...

---
+++ src/test/java/cn/snowpic/year_2020/day_01_30/GenericTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright(c) snowpic.cn 2019-2019.All rights reserved.
+ */
+
+package cn.snowpic.year_2020.day_01_30;
+
+import org.junit.Test;
+
+/**
+ * generic test
+ *
+ * @className GenericTest
+ * @author lf
+ * @time 2020-01-31 00:09:20
+ */
+public class GenericTest {
+
+    @Test
+    public void test1() {
+        String[] inputs1 = {"flower", "flow", "flight"};
+        String commonPrefix1 = findCommonPrefix(inputs1);
+        System.out.println("commonPrefix1 = " + commonPrefix1);
+    }
+
+    /**
+     * find common prefix
+     *
+     * @author lf
+     * @time 2020-01-31 00:09:09
+     * @param inputs inputs
+     * @return String
+     */
+    private String findCommonPrefix(String[] inputs) {
+        String result = null;
+        for (String input : inputs) {
+            if (result == null) {
+                result = input;
+            }
+            int len = Math.min(result.length(), input.length());
+            result = result.substring(0, len);
+            for (int i = 0; i < len; i++) {
+                if (result.charAt(i) != input.charAt(i)) {
+                    result = result.substring(0, i);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * test2
+     *
+     * @author lf
+     * @time 2020-01-31 00:20:02
+     */
+    @Test
+    public void test2() {
+        System.out.println("k".substring(0, 0));
+    }
+}
