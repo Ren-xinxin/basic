3395c28c4ec0c076fae23171e9a6832b74f05e55
Little Flower <snowpic@163.com>
2020-02-07 20:10:01 +0800

    reverse vowel algorithm....

--- src/test/java/cn/snowpic/year_2020/day_02_07/GenericTest.java
+++ src/test/java/cn/snowpic/year_2020/day_02_07/GenericTest.java
@@ -6,7 +6,9 @@ package cn.snowpic.year_2020.day_02_07;
 
 import org.junit.Test;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class GenericTest {
@@ -41,5 +43,55 @@ public class GenericTest {
 
         boolean construct2 = construct("baa", "aba");
         System.out.println("construct1 = " + construct2);
+    }
+
+    /**
+     * reverse vowel
+     *
+     * @author lf
+     * @time 2020-02-07 20:05:21
+     * @param input input
+     * @return String
+     */
+    private String reverseVowel(String input) {
+        Set<Character> vowel = new HashSet<Character>() {{
+            add('a');
+            add('e');
+            add('i');
+            add('o');
+            add('u');
+            add('A');
+            add('E');
+            add('I');
+            add('O');
+            add('U');
+        }};
+        char[] chars = input.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+        while (left < right) {
+            while (left < right && !vowel.contains(chars[left])) {
+                left++;
+            }
+            while (left < right && !vowel.contains(chars[right])) {
+                right--;
+            }
+            if (left < right) {
+                char tep = chars[left];
+                chars[left] = chars[right];
+                chars[right] = tep;
+                left++;
+                right--;
+            }
+        }
+        return new String(chars);
+    }
+
+    @Test
+    public void test2() {
+        String input = "LeetCode";
+
+        String reverseVowel = reverseVowel(input);
+        System.out.println("reverseVowel = " + reverseVowel);
     }
 }
