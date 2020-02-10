238eaca69bb76c27f40ff4e510a9fabb0822147d
Little Flower <snowpic@163.com>
2020-02-10 14:50:19 +0800

    incorrect commit...

--- src/test/java/cn/snowpic/year_2020/day_02_10/GenericTest.java
+++ src/test/java/cn/snowpic/year_2020/day_02_10/GenericTest.java
@@ -4,7 +4,13 @@
 
 package cn.snowpic.year_2020.day_02_10;
 
+import org.eclipse.jetty.util.BlockingArrayQueue;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class GenericTest {
 
@@ -52,5 +58,65 @@ public class GenericTest {
         String reverseWord = reverseWord(input);
 
         System.out.println("reverseWord = " + reverseWord);
+    }
+
+    static class Node {
+        private Node left;
+        private Node right;
+
+        private Integer value;
+
+        public Node(Integer value) {
+            this.value = value;
+        }
+
+        public Node(List<Integer> list) {
+            AtomicInteger index = new AtomicInteger();
+            value = list.get(index.get());
+            setChild(list, index, 1, new BlockingArrayQueue<>());
+        }
+
+        public void setChild(List<Integer> container, AtomicInteger index, int pow, Queue<Node> children) {
+            if (index.incrementAndGet() < container.size()) {
+                left = new Node(container.get(index.get()));
+                children.add(left);
+            }
+            if (index.incrementAndGet() < container.size()) {
+                right = new Node(container.get(index.get()));
+                children.add(right);
+            }
+            if (index.get() >= children.size() || children.size() == 2 << pow) {
+                Node node;
+                while ((node = children.poll()) != null) {
+                    node.setChild(container, index, pow + 1, new BlockingArrayQueue<>());
+                }
+            }
+        }
+
+        @Override
+        public String toString() {
+            return value == null ? "" :
+                    (value
+                            // left node leaf
+                            + (left == null ? "" : (left.value == null ? "()" : "(" + left + ")"))
+                            // right node leaf
+                            + (right == null ? "" : ("(" + right + ")"))
+                    );
+        }
+    }
+
+    @Test
+    public void test2() {
+        List<Integer> list = new ArrayList<Integer>() {{
+            add(1);
+            add(2);
+            add(3);
+            add(4);
+            add(5);
+            add(6);
+            add(7);
+        }};
+        Node node = new Node(list);
+        System.out.println("node = " + node);
     }
 }
