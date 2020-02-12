bcaeeb380ad75452c964fbfb4c79cbc844673266
Little Flower <snowpic@163.com>
2020-02-12 23:03:09 +0800

    count binary algorithm...

--- src/test/java/cn/snowpic/year_2020/day_02_12/GenericTest.java
+++ src/test/java/cn/snowpic/year_2020/day_02_12/GenericTest.java
@@ -6,6 +6,8 @@ package cn.snowpic.year_2020.day_02_12;
 
 import org.junit.Test;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class GenericTest {
 
     /**
@@ -36,5 +38,54 @@ public class GenericTest {
 
         int times = duplicateTimes(origin, target);
         System.out.println("times = " + times);
+    }
+
+    /**
+     * counter binary
+     *
+     * @author lf
+     * @time 2020-02-12 22:10:37
+     * @param input input
+     * @param eden eden
+     * @param counter counter
+     */
+    private void countBinary(String input, int eden, AtomicInteger counter) {
+        boolean isStart = true;
+        int tepCounter = 1;
+        for (int i = eden + 1; i < input.length(); i++) {
+            boolean isOrigin = input.charAt(i) == input.charAt(eden);
+            if (isStart && isOrigin) {
+                tepCounter++;
+            } else {
+                if (isOrigin) return;
+                if (isStart) isStart = false;
+                if (--tepCounter == 0) {
+                    counter.getAndIncrement();
+                }
+            }
+        }
+    }
+
+    /**
+     * count binary
+     *
+     * @author lf
+     * @time 2020-02-12 22:10:43
+     * @param input input
+     * @return int
+     */
+    private int countBinary(String input) {
+        AtomicInteger count = new AtomicInteger();
+        for (int i = 0; i < input.length(); i++) {
+            countBinary(input, i, count);
+        }
+        return count.get();
+    }
+
+    @Test
+    public void test2() {
+        String input = "00110011";
+        int count = countBinary(input);
+        System.out.println("count = " + count);
     }
 }
