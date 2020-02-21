0d0d6b868a3919f340a30aebdf3746e623955903
Little Flower <snowpic@163.com>
2020-02-21 23:53:36 +0800

    sorrow commit...

---
+++ src/test/java/cn/snowpic/year_2020/month_02/day_21/GenericTest.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright(c) snowpic.cn 2019-2020.All rights reserved.
+ */
+
+package cn.snowpic.year_2020.month_02.day_21;
+
+/**
+ * generic test
+ *
+ * @className GenericTest
+ * @author lf
+ * @time 2020-02-21 23:25:07
+ */
+public class GenericTest {
+
+    private void spilt2Ip(String input) {
+
+    }
+}
