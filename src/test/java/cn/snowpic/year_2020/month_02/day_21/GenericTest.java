fcfcadfc735676497bdd60bc7415b0a7a01a3222
Little Flower <snowpic@163.com>
2020-02-22 16:30:13 +0800

    restore ip string to ips algorithm...

--- src/test/java/cn/snowpic/year_2020/month_02/day_21/GenericTest.java
+++ src/test/java/cn/snowpic/year_2020/month_02/day_21/GenericTest.java
@@ -4,6 +4,12 @@
 
 package cn.snowpic.year_2020.month_02.day_21;
 
+import org.apache.commons.lang.StringUtils;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * generic test
  *
@@ -13,7 +19,63 @@ package cn.snowpic.year_2020.month_02.day_21;
  */
 public class GenericTest {
 
-    private void spilt2Ip(String input) {
+    /**
+     * spilt2 ip
+     *
+     * @author lf
+     * @time 2020-02-22 15:36:43
+     * @param input input
+     * @param index index
+     * @param dotCount dot count
+     * @param str str
+     * @param result result
+     */
+    private void spilt2Ip(String input, int index, int dotCount, String str, List<String> result) {
+        if (dotCount == 3) {
+            String last = input.substring(index);
+            if (!StringUtils.isBlank(last) && Integer.parseInt(last) < 256) {
+                result.add(str + last);
+            }
+            return;
+        }
+        int len = Math.min(index + 3, input.length());
+        for (int i = index; i < len; i++) {
+            // whether the start is zero.
+            if (Integer.parseInt(input.charAt(index) + "") == 0) {
+                return;
+            }
+            int nextIndex = i + 1;
+            String current = input.substring(index, nextIndex);
+            if (Integer.parseInt(current) > 255) {
+                return;
+            }
+            spilt2Ip(input, nextIndex, dotCount + 1, str + current + ".", result);
+        }
+    }
 
+    /**
+     * spit2 ip
+     *
+     * @author lf
+     * @time 2020-02-22 15:40:19
+     * @param input input
+     * @return @List<String>
+     */
+    private List<String> spit2Ip(String input) {
+        if (input.length() > 3 * 4) {
+            throw new IllegalArgumentException("Ip string's length must less than 3 * 4!");
+        }
+        List<String> result = new ArrayList<>();
+        spilt2Ip(input, 0, 0, "", result);
+        return result;
+    }
+
+    @Test
+    public void test1() {
+        String input = "25525511135";
+        List<String> list = spit2Ip(input);
+        System.out.println("list = " + list);
+
+        System.out.println(spit2Ip("2322133"));
     }
 }
