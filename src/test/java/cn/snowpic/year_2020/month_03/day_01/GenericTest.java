14bf0a82fa4ac24eaa0aebcf74280ab74807a414
Little Flower <snowpic@163.com>
2020-03-01 16:22:03 +0800

    brackets validate algorithm...

--- src/test/java/cn/snowpic/year_2020/month_03/day_01/GenericTest.java
+++ src/test/java/cn/snowpic/year_2020/month_03/day_01/GenericTest.java
@@ -7,6 +7,8 @@ package cn.snowpic.year_2020.month_03.day_01;
 import cn.snowpic.util.CommonUtils;
 import org.junit.Test;
 
+import java.util.Stack;
+
 /**
  * generic test
  *
@@ -41,5 +43,86 @@ public class GenericTest {
     public void test1() {
         int count = palindromeCount("aaa");
         System.out.println("count = " + count);
+    }
+
+    /**
+     * validate
+     *
+     * @author lf
+     * @time 2020-03-01 16:07:39
+     * @param origin origin
+     * @param container container
+     * @param currentIndex current index
+     * @param current current
+     * @return boolean
+     */
+    private boolean validate(String origin, Stack<Character> container, int currentIndex, Character current) {
+        if (currentIndex == origin.length()) {
+            return container.isEmpty();
+        }
+        if (current == null && origin.charAt(currentIndex) == '*') {
+            boolean validate1 = validate(origin, stackClone(container), currentIndex, '(');
+            boolean validate2 = validate(origin, stackClone(container), currentIndex, ')');
+            // any character will be ok
+            boolean validate3 = validate(origin, stackClone(container), currentIndex, '-');
+            return validate1 || validate2 || validate3;
+        } else {
+            current = current == null ? origin.charAt(currentIndex) : current;
+            switch (current) {
+                case '(':
+                    container.push(current);
+                    break;
+                case ')':
+                    if (container.isEmpty()) return false;
+                    container.pop();
+                    break;
+                case '-':
+                    break;
+                default:
+                    return false;
+            }
+            return validate(origin, container, currentIndex + 1, null);
+        }
+    }
+
+    /**
+     * stack clone
+     *
+     * @author lf
+     * @time 2020-03-01 16:02:35
+     * @param origin origin
+     * @return @Stack<Character>
+     */
+    @SuppressWarnings("unchecked")
+    private Stack<Character> stackClone(Stack<Character> origin) {
+        return (Stack<Character>) (origin.clone());
+    }
+
+    /**
+     * validate
+     *
+     * @author lf
+     * @time 2020-03-01 16:08:39
+     * @param input input
+     * @return boolean
+     */
+    private boolean validate(String input) {
+        if (!input.matches("^[()*]*$")) throw new IllegalArgumentException("Input is incorrect.");
+        return validate(input, new Stack<>(), 0, null);
+    }
+
+    @Test
+    public void test2() {
+        boolean validate1 = validate("");
+        System.out.println("validate1 = " + validate1);
+
+        boolean validate2 = validate("()");
+        System.out.println("validate1 = " + validate2);
+
+        boolean validate3 = validate("(*)");
+        System.out.println("validate1 = " + validate3);
+
+        boolean validate4 = validate("(*))");
+        System.out.println("validate1 = " + validate4);
     }
 }
