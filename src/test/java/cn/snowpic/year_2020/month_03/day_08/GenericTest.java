b1ef40081318473e6531032b2d0b5b2b878b1645
Little Flower <snowpic@163.com>
2020-03-08 20:39:16 +0800

    optimize black jack algorithm...

--- src/test/java/cn/snowpic/year_2020/month_03/day_08/GenericTest.java
+++ src/test/java/cn/snowpic/year_2020/month_03/day_08/GenericTest.java
@@ -4,9 +4,10 @@
 
 package cn.snowpic.year_2020.month_03.day_08;
 
+import org.apache.curator.shaded.com.google.common.util.concurrent.AtomicDouble;
 import org.junit.Test;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.text.DecimalFormat;
 
 /**
  * generic test
@@ -25,15 +26,17 @@ public class GenericTest {
      * @param originLimit origin limit
      * @param stopLimit stop limit
      * @param targetLimit target limit
-     * @return double
+     * @return String
      */
-    private double blackJack(final int originLimit, final int stopLimit, final int targetLimit) {
+    private String blackJack(final int originLimit, final int stopLimit, final int targetLimit) {
         if (stopLimit < 0 || originLimit < 0 || targetLimit < 0)
             throw new IllegalArgumentException("Params is illegal.");
-        AtomicInteger targetCount = new AtomicInteger();
-        AtomicInteger allCount = new AtomicInteger();
-        play(originLimit, stopLimit, targetLimit, 0, targetCount, allCount);
-        return targetCount.get() * 1.0 / allCount.get();
+        AtomicDouble targetCount = new AtomicDouble();
+        AtomicDouble allCount = new AtomicDouble();
+        play(originLimit, stopLimit, targetLimit, 0, targetCount, allCount, 1.0);
+        double result = targetCount.get() / allCount.get();
+        DecimalFormat sf = new DecimalFormat("0.00000");
+        return sf.format(result);
     }
 
     /**
@@ -48,30 +51,30 @@ public class GenericTest {
      * @param targetCount target count
      * @param allCount all count
      */
-    private void play(final int originLimit, final int stopLimit, final int targetLimit, int currentTotal, AtomicInteger targetCount, AtomicInteger allCount) {
+    private void play(final int originLimit, final int stopLimit, final int targetLimit, int currentTotal, AtomicDouble targetCount, AtomicDouble allCount, double weight) {
         if (currentTotal > 0) {
             if (currentTotal >= stopLimit) {
                 if (currentTotal <= targetLimit) {
-                    targetCount.getAndIncrement();
+                    targetCount.getAndAdd(weight);
                 }
-                allCount.getAndIncrement();
+                allCount.getAndAdd(weight);
                 return;
             }
         }
         for (int i = 1; i <= originLimit; i++) {
-            play(originLimit, stopLimit, targetLimit, currentTotal + i, targetCount, allCount);
+            play(originLimit, stopLimit, targetLimit, currentTotal + i, targetCount, allCount, weight / 10);
         }
     }
 
     @Test
     public void test1() {
-        double jack = blackJack(10, 1, 10);
+        String jack = blackJack(10, 1, 10);
         System.out.println("jack = " + jack);
 
-        double jack1 = blackJack(10, 1, 6);
+        String jack1 = blackJack(10, 1, 6);
         System.out.println("jack1 = " + jack1);
 
-        double jack2 = blackJack(3, 4, 4);
+        String jack2 = blackJack(10, 17, 21);
         System.out.println("jack2 = " + jack2);
     }
 }
