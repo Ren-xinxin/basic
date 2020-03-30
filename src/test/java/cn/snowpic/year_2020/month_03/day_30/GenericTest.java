7fdec6d5290fc1e1fa929bfea2fa849606e315b6
Little Flower <snowpic@163.com>
2020-03-30 23:28:40 +0800

    incorrect commit...

--- src/test/java/cn/snowpic/year_2020/month_03/day_30/GenericTest.java
+++ src/test/java/cn/snowpic/year_2020/month_03/day_30/GenericTest.java
@@ -6,6 +6,8 @@ package cn.snowpic.year_2020.month_03.day_30;
 
 import org.junit.Test;
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -56,5 +58,82 @@ public class GenericTest {
     private void print() {
         char str = (char) (ORIGIN + counter.getAndIncrement());
         System.out.println(Thread.currentThread().getName() + ":" + str);
+    }
+
+    @Test
+    public void test2() {
+        System.out.println(computeScore("()"));
+        System.out.println(computeScore("(())"));
+        System.out.println(computeScore("()()"));
+        System.out.println(computeScore("(()(()))"));
+    }
+
+    /**
+     * resolve score
+     *
+     * @author Little Flower
+     * @date 2020-03-30 22:48:02
+     * @param input input
+     * @param start start
+     * @return int
+     */
+    private int resolveScore(String input, AtomicInteger start) {
+        int pow = -1;
+        int sum = 0;
+        boolean flag = false;
+        for (int i = start.get() + 1; i < input.length(); i++) {
+            start.set(i);
+            char curr = input.charAt(i);
+            if (curr == ')') {
+                pow++;
+                flag = true;
+            } else if (curr == '(' && flag) {
+                sum = (int) Math.pow(2, pow);
+                pow = -1;
+                sum += resolveScore(input, start);
+                i = start.get();
+            }
+        }
+        return sum;
+    }
+
+    /**
+     * compute score
+     *
+     * @author Little Flower
+     * @date 2020-03-30 22:47:55
+     * @param input input
+     * @return int
+     */
+    private int computeScore(String input) {
+        checkFormat(input);
+        return resolveScore(input, new AtomicInteger(0));
+    }
+
+    /**
+     * check format
+     *
+     * @author Little Flower
+     * @date 2020-03-30 22:28:01
+     * @param input input
+     */
+    private void checkFormat(String input) {
+        if (input.length() == 0) {
+            throw new IllegalArgumentException("Input's format is illegal.");
+        }
+        Deque<Character> deque = new LinkedList<>();
+        for (int i = 0; i < input.length(); i++) {
+            char curr = input.charAt(i);
+            if (curr == '(') {
+                deque.push('(');
+            } else if (!deque.isEmpty() && curr == ')') {
+                deque.pop();
+            } else {
+                throw new IllegalArgumentException("Input's format is illegal.");
+            }
+        }
+        if (!deque.isEmpty()) {
+            throw new IllegalArgumentException("Input's format is illegal.");
+        }
     }
 }
