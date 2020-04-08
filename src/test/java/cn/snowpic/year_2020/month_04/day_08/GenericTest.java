18fef06bde119d711829005faff8db4859133f47
Little Flower <snowpic@163.com>
2020-04-09 00:19:43 +0800

    fix bug...

--- src/test/java/cn/snowpic/year_2020/month_04/day_08/GenericTest.java
+++ src/test/java/cn/snowpic/year_2020/month_04/day_08/GenericTest.java
@@ -4,49 +4,76 @@
 
 package cn.snowpic.year_2020.month_04.day_08;
 
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class GenericTest {
 
     private int minSteps(String origin, final String target) {
         int max = Integer.MIN_VALUE;
-        int originLeft = 0, originRight = 0, targetLeft = 0, targetRight = 0;
+        StepInfo result = null;
         for (int i = 0; i < target.length(); i++) {
             for (int j = 0; j < origin.length(); j++) {
-                if (origin.charAt(j) == target.charAt(i)) {
-                    int ol = j - 1, or = j + 1, tl = i - 1, tr = i + 1;
-                    int left = j, right = j;
-                    int used = 1;
-                    while (ol >= 0 && tl >= 0) {
-                        if (origin.charAt(ol) == target.charAt(tl)) {
-                            used++;
-                            left = ol;
-                            tl--;
-                        }
-                        ol--;
-                    }
-                    while (or < origin.length() && tr < target.length()) {
-                        if (origin.charAt(or) == target.charAt(tr)) {
-                            used++;
-                            right = or;
-                            tr++;
-                        }
-                        or++;
-                    }
-                    if (used > max) {
-                        max = used;
-
-                        originLeft = left;
-                        originRight = right;
-
-                        targetLeft = tl + 1;
-                        targetRight = tr - 1;
-                    }
+                StepInfo info = new StepInfo();
+                int steps = multiplexElements(origin, target, j, i, 0, new AtomicBoolean(true), info);
+                if (steps > max) {
+                    max = steps;
+                    result = info;
                 }
             }
         }
-
-        int left = Math.max(originLeft, targetLeft);
-        int middle = (originRight - originLeft + 1) - (targetRight - targetLeft + 1);
-        int right = Math.max(origin.length() - 1 - originRight, target.length() - 1 - targetRight);
+        if (result == null) throw new RuntimeException("sb");
+        int left = Math.max(result.ol, result.tl);
+        int middle = Math.abs((result.or - result.ol + 1) - (result.tr - result.tl + 1));
+        int right = Math.max(origin.length() - 1 - result.or, target.length() - 1 - result.tr);
         return left + middle + right;
+    }
+
+    /**
+     * multiplex elements
+     *
+     * @author Little Flower
+     * @date 2020-04-09 00:14:20
+     * @param origin origin
+     * @param target target
+     * @param oi oi
+     * @param ti ti
+     * @param currUsed curr used
+     * @param isLeft is left
+     * @param stepInfo step info
+     * @return int
+     */
+    private int multiplexElements(final String origin, final String target, int oi, int ti, int currUsed, AtomicBoolean isLeft, StepInfo stepInfo) {
+        if (oi == origin.length() || ti == target.length()) {
+            return currUsed;
+        }
+        if (origin.charAt(oi) == target.charAt(ti)) {
+            if (isLeft.get()) {
+                isLeft.set(false);
+                stepInfo.ol = oi;
+                stepInfo.tl = ti;
+            }
+            stepInfo.or = oi;
+            stepInfo.tr = ti;
+            return multiplexElements(origin, target, oi + 1, ti + 1, currUsed + 1, isLeft, stepInfo);
+        } else {
+            int e1 = multiplexElements(origin, target, oi + 1, ti, currUsed, isLeft, stepInfo);
+            int e2 = multiplexElements(origin, target, oi, ti + 1, currUsed, isLeft, stepInfo);
+            return Math.max(e1, e2);
+        }
+    }
+
+    private static class StepInfo {
+        int ol;
+        int or;
+        int tl;
+        int tr;
+    }
+
+    @Test
+    public void test1() {
+        int steps = minSteps("ac", "abc");
+        System.out.println("steps = " + steps);
     }
 }
