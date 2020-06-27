f7e85de703ee26c6790cf9913b0ab2d572e29635
Little Flower <snsowpic@163.com>
2020-06-27 23:48:24 +0800

    available processors...

---
+++ src/test/java/cn/snowpic/year_2020/month_06/day_27/GenericTest.java
@@ -0,0 +1,18 @@
+package cn.snowpic.year_2020.month_06.day_27;
+
+import org.junit.Test;
+
+/**
+ * generic test
+ *
+ * @className GenericTest
+ * @author Little Flower
+ * @date 2020-06-27 23:27:24
+ */
+public class GenericTest {
+
+    @Test
+    public void test1() {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
