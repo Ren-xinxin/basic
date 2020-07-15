01b500eda107b2bdb7ddf8a4ea9674b18e68d1c2
Little Flower <snowpic@163.com>
2020-07-15 21:08:41 +0800

    just commit...

--- src/test/java/cn/snowpic/year_2020/month_06/day_12/GenericTest.java
+++ src/test/java/cn/snowpic/year_2020/month_06/day_12/GenericTest.java
@@ -7,18 +7,19 @@ package cn.snowpic.year_2020.month_06.day_12;
 import cn.snowpic.model.RequestVo;
 import cn.snowpic.util.DisableSslUtils;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
 
 public class GenericTest {
 
     @Test
-    public void test1() throws IOException, KeyManagementException, NoSuchAlgorithmException {
+    public void test1() throws IOException {
         Connection connection = Jsoup.connect("https://127.0.0.1:8081/provider/hello-api/ok");
         DisableSslUtils.disableSslVerification();
         RequestVo vo = RequestVo.builder()
@@ -29,6 +30,14 @@ public class GenericTest {
 
 
         String params = JSON.toJSONString(vo);
+
+        RequestVo vo1 = JSONObject.parseObject(params, RequestVo.class);
+
+        ObjectMapper mapper = new ObjectMapper()
+                .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        RequestVo vo2 = mapper.readValue(params, RequestVo.class);
 
         Connection.Response response = connection.method(Connection.Method.POST)
                 .header("Content-Type", "application/json")
---
+++ src/test/java/cn/snowpic/year_2020/month_07/day_15/GenericTest.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright(c) snowpic.cn 2019-2020. All rights reserved.
+ */
+
+package cn.snowpic.year_2020.month_07.day_15;
+
+import org.junit.Test;
+
+public class GenericTest {
+
+    @Test
+    public void test1() {
+        System.out.println("replace old computer's cooler material..");
+    }
+}
