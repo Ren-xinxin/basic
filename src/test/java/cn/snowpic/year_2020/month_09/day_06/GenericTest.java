46cbba6c1a458cb2be540f68c581fc1a4e2be4c9
Little Flower <snsowpic@163.com>
2020-09-06 20:31:23 +0800

    collection.removeAll() only invoke equals method...

--- src/test/java/cn/snowpic/year_2020/month_09/day_06/GenericTest.java
+++ src/test/java/cn/snowpic/year_2020/month_09/day_06/GenericTest.java
@@ -6,8 +6,11 @@ package cn.snowpic.year_2020.month_09.day_06;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class GenericTest {
     private static class Aa {
@@ -35,5 +38,14 @@ public class GenericTest {
         final Aa e1 = new Aa();
         e1.age = "22";
         set.add(e);
+
+        final ArrayList<Aa> list = new ArrayList<>();
+        list.add(e);
+        list.add(e1);
+
+        final List<Aa> collect = list.stream().distinct().collect(Collectors.toList());
+        final Aa o = new Aa();
+        o.age="66";
+        list.remove(o);
     }
 }
