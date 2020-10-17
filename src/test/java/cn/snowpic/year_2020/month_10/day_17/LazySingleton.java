bf85902d0b37d3319bfd87ccfaf7f083614924a7
Little Flower <snsowpic@163.com>
2020-10-17 21:04:09 +0800

    hungry and lazy for singleton...

---
+++ src/test/java/cn/snowpic/year_2020/month_10/day_17/LazySingleton.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright(c) snowpic.cn 2019-2020. All rights reserved.
+ */
+
+package cn.snowpic.year_2020.month_10.day_17;
+
+public class LazySingleton {
+    private static LazySingleton instance;
+
+    private LazySingleton() {
+    }
+
+    public static synchronized LazySingleton getInstance() {
+        if (instance == null) {
+            instance = new LazySingleton();
+        }
+        return instance;
+    }
+}
