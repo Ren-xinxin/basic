d723b00143e3463a04b09c5c1d753802ee13f3e7
Little Flower <389175923@qq.com>
2020-11-01 19:39:08 +0800

    search insert position algorithm...

---
+++ src/test/java/cn/snowpic/year_2020/month_11/day_01/GenericTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright(c) snowpic.cn 2019-2020. All rights reserved.
+ */
+
+package cn.snowpic.year_2020.month_11.day_01;
+
+import org.junit.Test;
+
+public class GenericTest {
+
+    @Test
+    public void test1() {
+        int[] nums = {1, 3, 5, 6};
+        int index1 = searchInsertedIndex(nums, 5);
+        System.out.println(index1);
+
+        int index2 = searchInsertedIndex(nums, 2);
+        System.out.println(index2);
+    }
+
+    private int searchInsertedIndex(int[] nums, int target) {
+        if (nums[0] >= target) {
+            return 0;
+        }
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] < target && nums[i] >= target) {
+                return i;
+            }
+        }
+
+        return nums.length;
+    }
+}
