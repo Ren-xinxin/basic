335f1b6b45818d9bc36deccaab68c4ac0dfdce06
Little Flower <389175923@qq.com>
2020-11-03 19:55:49 +0800

    plus one algorithm...

---
+++ src/test/java/cn/snowpic/year_2020/month_11/day_03/GenericTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright(c) snowpic.cn 2019-2020. All rights reserved.
+ */
+
+package cn.snowpic.year_2020.month_11.day_03;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class GenericTest {
+
+    @Test
+    public void test1() {
+        int[] nums1 = {1, 2, 3};
+        int[] result1 = plusOne(nums1);
+        System.out.println(Arrays.toString(result1));
+
+        int[] nums2 = {9, 9, 9};
+        int[] result2 = plusOne(nums2);
+        System.out.println(Arrays.toString(result2));
+    }
+
+    private int[] plusOne(int[] nums) {
+        int added = 0;
+        // plus one
+        nums[nums.length - 1] += 1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int curr = nums[i] + added;
+            nums[i] = curr % 10;
+            added = curr / 10;
+        }
+        if (added == 0) {
+            return nums;
+        }
+        int[] result = new int[nums.length + 1];
+        result[0] = added;
+        System.arraycopy(nums, 0, result, 1, nums.length);
+
+        return result;
+    }
+}
