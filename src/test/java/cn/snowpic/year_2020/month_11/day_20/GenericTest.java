c30ec22bd125d993c4bb64b8e02a304a0431f878
Little Flower <389175923@qq.com>
2020-11-20 22:58:56 +0800

    delete incorrect method...

--- src/test/java/cn/snowpic/year_2020/month_11/day_20/GenericTest.java
+++ src/test/java/cn/snowpic/year_2020/month_11/day_20/GenericTest.java
@@ -6,11 +6,8 @@ package cn.snowpic.year_2020.month_11.day_20;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class GenericTest {
 
@@ -18,34 +15,6 @@ public class GenericTest {
     public void test1() {
         System.out.println(findDuplicates2(new int[]{1, 2, 3, 1}, 3));
         System.out.println(findDuplicates2(new int[]{1, 2, 3, 1, 2, 3}, 2));
-    }
-
-    private boolean findDuplicates(int[] array, int distance) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < array.length; i++) {
-            int key = array[i];
-            List<Integer> indices = map.get(key);
-            if (indices == null) {
-                indices = new ArrayList<>();
-            }
-            indices.add(i);
-            map.put(key, indices);
-        }
-        List<List<Integer>> duplicatedIndices = map.values().stream().filter(m -> (m != null && m.size() > 1))
-                .collect(Collectors.toList());
-        for (List<Integer> indices : duplicatedIndices) {
-            for (Integer index : indices) {
-                int len = distance + index;
-                if (len < array.length) {
-                    if (indices.contains(len)) {
-                        return true;
-                    }
-                } else {
-                    break;
-                }
-            }
-        }
-        return false;
     }
 
     private boolean findDuplicates2(int[] array, int distance) {
