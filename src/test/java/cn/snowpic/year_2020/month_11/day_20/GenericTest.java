e1ffccd8b6dfbb59087663534b830ec37e84d05d
Little Flower <389175923@qq.com>
2020-11-20 22:57:56 +0800

    correct algorithm...

--- src/test/java/cn/snowpic/year_2020/month_11/day_20/GenericTest.java
+++ src/test/java/cn/snowpic/year_2020/month_11/day_20/GenericTest.java
@@ -16,8 +16,8 @@ public class GenericTest {
 
     @Test
     public void test1() {
-        System.out.println(findDuplicates(new int[]{1, 2, 3, 1}, 3));
-        System.out.println(findDuplicates(new int[]{1, 2, 3, 1, 2, 3}, 2));
+        System.out.println(findDuplicates2(new int[]{1, 2, 3, 1}, 3));
+        System.out.println(findDuplicates2(new int[]{1, 2, 3, 1, 2, 3}, 2));
     }
 
     private boolean findDuplicates(int[] array, int distance) {
@@ -43,6 +43,24 @@ public class GenericTest {
                 } else {
                     break;
                 }
+            }
+        }
+        return false;
+    }
+
+    private boolean findDuplicates2(int[] array, int distance) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            int key = array[i];
+            Integer oldIndex = map.get(key);
+            if (oldIndex != null) {
+                if (i - oldIndex <= distance) {
+                    return true;
+                } else {
+                    map.put(key, i);
+                }
+            } else {
+                map.put(key, i);
             }
         }
         return false;
