e28108b7b497846769955e5f5e2c0cfbe7df1dc8
Little Flower <389175923@qq.com>
2021-01-30 20:59:42 +0800

    decimal transfer to any base...

--- src/test/java/cn/snowpic/year_2021/month_01/day_26/GenericTest.java
+++ src/test/java/cn/snowpic/year_2021/month_01/day_26/GenericTest.java
@@ -4,14 +4,11 @@
 
 package cn.snowpic.year_2021.month_01.day_26;
 
-import org.junit.Test;
-
 import java.util.Scanner;
 
 public class GenericTest {
 
-    @Test
-    public void test1() {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
             String line = scanner.nextLine();
@@ -26,8 +23,17 @@ public class GenericTest {
         }
     }
 
-    private String convert(double num, int base) {
+    private static String convert(double num, int base) {
         int[] arr = new int[10];
-        return null;
+        for (int i = 0; i < arr.length; i++) {
+            num *= base;
+            arr[i] = (int) num;
+            num -= arr[i];
+        }
+        StringBuilder sb = new StringBuilder("0.");
+        for (int i : arr) {
+            sb.append(i);
+        }
+        return sb.toString();
     }
 }
