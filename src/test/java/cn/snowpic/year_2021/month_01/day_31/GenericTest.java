1ec66cb7ff0518bb569351c692dc33554f02e996
Little Flower <389175923@qq.com>
2021-01-30 22:45:55 +0800

    find k-diff pairs...

--- src/test/java/cn/snowpic/year_2021/month_01/day_31/GenericTest.java
+++ src/test/java/cn/snowpic/year_2021/month_01/day_31/GenericTest.java
@@ -6,6 +6,11 @@ package cn.snowpic.year_2021.month_01.day_31;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class GenericTest {
 
     @Test
@@ -26,5 +31,41 @@ public class GenericTest {
             max = Math.max(count, max);
         }
         return max;
+    }
+
+    @Test
+    public void test2() {
+        int num = findKDiffPairs(new int[]{3, 1, 4, 1, 5,}, 2);
+        System.out.println(num);
+        System.out.println(findKDiffPairs(new int[]{1, 2, 3, 4, 5,}, 1));
+        System.out.println(findKDiffPairs(new int[]{1, 3, 1, 5, 4,}, 0));
+    }
+
+    private int findKDiffPairs(int[] array, int k) {
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            int key = array[i];
+            Set<Integer> value = map.get(key);
+            if (value == null) {
+                value = new HashSet<>();
+            }
+            value.add(i);
+            map.put(key, value);
+        }
+        int count = 0;
+        for (Integer integer : map.keySet()) {
+            int key = integer + k;
+            Set<Integer> value = map.get(key);
+            if (k == 0) {
+                if (value.size() > 1) {
+                    count++;
+                }
+                continue;
+            }
+            if (value != null) {
+                count++;
+            }
+        }
+        return count;
     }
 }
