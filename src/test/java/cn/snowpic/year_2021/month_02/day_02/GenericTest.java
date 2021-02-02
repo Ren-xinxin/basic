886121aa699fc448e4b2c1fc3de40aa0e00c76b9
Little Flower <389175923@qq.com>
2021-02-02 22:22:07 +0800

    find unsorted subarray algorithm...

---
+++ src/test/java/cn/snowpic/year_2021/month_02/day_02/GenericTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright(c) snowpic.cn 2019-2021. All rights reserved.
+ */
+
+package cn.snowpic.year_2021.month_02.day_02;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class GenericTest {
+    @Test
+    public void test1() {
+        int len = findMinSubarray(new int[]{2, 6, 4, 8, 10, 9, 15});
+        System.out.println(len);
+    }
+
+    private int findMinSubarray(int[] array) {
+        int[] copy = new int[array.length];
+        System.arraycopy(array, 0, copy, 0, array.length);
+        Arrays.sort(copy);
+        int start = 0;
+        int end = array.length - 1;
+        boolean isStart = true;
+        for (int i = 0; i < array.length; i++) {
+            if (isStart && array[i] != copy[i]) {
+                start = i;
+                isStart = false;
+            }
+            if (!isStart && array[i] == copy[i]) {
+                end = i - 1;
+            }
+        }
+        return end - start + 1;
+    }
+}
