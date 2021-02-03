4457091207eb0d1b2a64fff75ba4e5d936feeecd
Little Flower <389175923@qq.com>
2021-02-03 22:16:31 +0800

    optimize code...

--- src/test/java/cn/snowpic/year_2021/month_02/day_03/GenericTest.java
+++ src/test/java/cn/snowpic/year_2021/month_02/day_03/GenericTest.java
@@ -32,6 +32,12 @@ public class GenericTest {
     }
 
     private int getGcd(int num1, int num2) {
+        if (num2 == 0) {
+            return num1;
+        }
+        if (num1 < 0 || num2 < 0) {
+            return -1;
+        }
         if (num2 > num1) {
             return getGcd(num2, num1);
         }
