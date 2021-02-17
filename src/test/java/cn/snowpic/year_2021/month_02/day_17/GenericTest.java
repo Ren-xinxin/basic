50aab50dffc556149043349dd74c485d2f90ceb0
Little Flower <snowpic@163.com>
2021-02-17 22:14:35 +0800

    check toeplitz algorithm...

--- src/test/java/cn/snowpic/year_2021/month_02/day_17/GenericTest.java
+++ src/test/java/cn/snowpic/year_2021/month_02/day_17/GenericTest.java
@@ -25,4 +25,33 @@ public class GenericTest {
         return Arrays.stream(array)
                 .anyMatch(num -> num != 0 && num != finalMax && finalMax / num < 2) ? -1 : index;
     }
+
+    @Test
+    public void test2() {
+        System.out.println(checkToeplitz(new int[][]{{1, 2, 3, 4}, {5, 1, 2, 3}, {9, 5, 1, 2}}));
+        System.out.println(checkToeplitz(new int[][]{{1, 2}, {2, 2}}));
+    }
+
+    private boolean checkToeplitz(int[][] matrix) {
+        int xLen = matrix.length;
+        int yLen = matrix[0].length;
+        int y = yLen - 1;
+        int x = 0;
+        while (x < xLen) {
+            int curr = matrix[x][y];
+            int tempX = x;
+            int tempY = y;
+            while (++tempX < xLen && ++tempY < yLen) {
+                if (matrix[tempX][tempY] != curr) {
+                    return false;
+                }
+            }
+            if (y > 0) {
+                y--;
+            } else {
+                x++;
+            }
+        }
+        return true;
+    }
 }
