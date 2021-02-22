1e54dc9e7300ad1446a23be55a9f341d2510005d
Little Flower <snowpic@163.com>
2021-02-22 22:42:41 +0800

    check monotone algorithm...

---
+++ src/test/java/cn/snowpic/year_2021/month_02/day_22/GenericTest.java
@@ -0,0 +1,40 @@
+package cn.snowpic.year_2021.month_02.day_22;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class GenericTest {
+
+    @Test
+    public void test1() {
+        System.out.println(checkMonotone(new int[]{1, 2, 2, 3}));
+        System.out.println(checkMonotone(new int[]{6, 5, 4, 4}));
+        System.out.println(checkMonotone(new int[]{1, 3, 2}));
+    }
+
+    private boolean checkMonotone(int[] array) {
+        int[] clone = array.clone();
+        Arrays.sort(clone);
+        int index = 0;
+        boolean flag = true;
+        for (int i : array) {
+            if (i != clone[index++]) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            return true;
+        }
+        flag = true;
+        index = array.length - 1;
+        for (int i : array) {
+            if (i != clone[index--]) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+}
