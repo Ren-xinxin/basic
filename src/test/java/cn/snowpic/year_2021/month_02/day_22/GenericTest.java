c61c70bd92c970b9f003aebd9666c4373c04f63c
Little Flower <snowpic@163.com>
2021-02-22 23:06:59 +0800

    maybe better for checking monotone algorithm...

--- src/test/java/cn/snowpic/year_2021/month_02/day_22/GenericTest.java
+++ src/test/java/cn/snowpic/year_2021/month_02/day_22/GenericTest.java
@@ -11,6 +11,10 @@ public class GenericTest {
         System.out.println(checkMonotone(new int[]{1, 2, 2, 3}));
         System.out.println(checkMonotone(new int[]{6, 5, 4, 4}));
         System.out.println(checkMonotone(new int[]{1, 3, 2}));
+        System.out.println("===============================");
+        System.out.println(checkMonotone2(new int[]{1, 2, 2, 3}));
+        System.out.println(checkMonotone2(new int[]{6, 5, 4, 4}));
+        System.out.println(checkMonotone2(new int[]{1, 3, 2}));
     }
 
     private boolean checkMonotone(int[] array) {
@@ -31,6 +35,27 @@ public class GenericTest {
         for (int i : array) {
             if (i != clone[index--]) {
                 return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkMonotone2(int[] array) {
+        return checkMonotone(array, true);
+    }
+
+    private boolean checkMonotone(int[] array, boolean flag) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (flag) {
+                // decreasing
+                if (array[i] < array[i + 1]) {
+                    return checkMonotone(array, false);
+                }
+            } else {
+                // increasing
+                if (array[i] > array[i + 1]) {
+                    return false;
+                }
             }
         }
         return true;
