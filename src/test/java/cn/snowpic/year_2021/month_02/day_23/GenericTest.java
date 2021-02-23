bfcf7e37fbddbf6ad5217f220467e6ba6e1f82e2
Little Flower <snowpic@163.com>
2021-02-23 22:30:41 +0800

    calculate expression algorithm...

--- src/test/java/cn/snowpic/year_2021/month_02/day_23/GenericTest.java
+++ src/test/java/cn/snowpic/year_2021/month_02/day_23/GenericTest.java
@@ -2,7 +2,10 @@ package cn.snowpic.year_2021.month_02.day_23;
 
 import org.junit.Test;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GenericTest {
     @Test
@@ -44,6 +47,50 @@ public class GenericTest {
             } else {
                 result[right--] = num;
             }
+        }
+        return result;
+    }
+
+    @Test
+    public void test2() {
+        System.out.println(calc("2*3-4+(-5)"));
+        System.out.println(calc("2*3-4+(1/5-5)"));
+        System.out.println(calc("1+2+3"));
+    }
+
+    private String calc(String expression) {
+        DecimalFormat df = new DecimalFormat("0.##");
+        Pattern pattern = Pattern.compile("\\(([^()]+)\\)");
+        while (true) {
+            Matcher matcher = pattern.matcher(expression);
+            if (!matcher.find()) {
+                return df.format(calcStr(expression));
+            }
+            String group = matcher.group(1);
+            expression = matcher.replaceFirst(String.valueOf(calcStr(group)));
+        }
+    }
+
+    private double calcStr(String expr) {
+        expr = expr.replaceAll("[ ]+", "");
+        expr = expr.replaceAll("\\+-|-\\+", "-");
+        expr = expr.replaceAll("-", "+-");
+        return Arrays.stream(expr.split("\\+")).filter(str -> str != null && str.length() != 0)
+                .map(this::getSecondValue).mapToDouble(Double::doubleValue).sum();
+    }
+
+    private double getSecondValue(String str) {
+        String[] operations = str.replaceAll("([*/])", " $1 ").split(" ");
+        double result = Double.parseDouble(operations[0]);
+        for (int index = 1; index < operations.length; index++) {
+            double next = Double.parseDouble(operations[index + 1]);
+            if ("*".equals(operations[index])) {
+                result *= next;
+            }
+            if ("/".equals(operations[index])) {
+                result /= next;
+            }
+            index++;
         }
         return result;
     }
