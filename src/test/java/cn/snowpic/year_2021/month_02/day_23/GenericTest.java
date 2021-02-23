ceb699f84bffc781b21ceecacee58b9549be2129
Little Flower <snowpic@163.com>
2021-02-23 21:30:01 +0800

    stones from other hills, just so so algorithm...

--- src/test/java/cn/snowpic/year_2021/month_02/day_23/GenericTest.java
+++ src/test/java/cn/snowpic/year_2021/month_02/day_23/GenericTest.java
@@ -9,6 +9,9 @@ public class GenericTest {
     public void test1() {
         System.out.println(Arrays.toString(sortByParity(new int[]{3, 1, 2, 4})));
         System.out.println(Arrays.toString(sortByParity(new int[]{4, 5, 1, 3})));
+        System.out.println("====================");
+        System.out.println(Arrays.toString(sortByParity2(new int[]{3, 1, 2, 4})));
+        System.out.println(Arrays.toString(sortByParity2(new int[]{4, 5, 1, 3})));
     }
 
     private int[] sortByParity(int[] array) {
@@ -27,5 +30,18 @@ public class GenericTest {
             }
         }
         return array;
+    }
+
+    private int[] sortByParity2(int[] array) {
+        int[] result = new int[array.length];
+        int left = 0, right = array.length - 1;
+        for (int num : array) {
+            if (num % 2 == 0) {
+                array[left++] = num;
+            } else {
+                array[right--] = num;
+            }
+        }
+        return result;
     }
 }
