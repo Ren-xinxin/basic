658cd5d0f5ff1d290803d96fa13bfaa30a78f823
Little Flower <snowpic@163.com>
2021-03-06 00:47:31 +0800

    incorrect commit...

---
+++ src/test/java/cn/snowpic/year_2021/month_03/day_05/GenericTest.java
@@ -0,0 +1,43 @@
+package cn.snowpic.year_2021.month_03.day_05;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class GenericTest {
+    @Test
+    public void test1() {
+        int[][] img = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        rotateImage(img);
+        System.out.println(Arrays.deepToString(img));
+        int[][] img2 = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+        rotateImage(img2);
+        System.out.println(Arrays.deepToString(img2));
+    }
+
+    private void rotateImage(int[][] img) {
+        int len = img.length - 1;
+        int row = 0;
+        while (len > 0) {
+            int column = row;
+            while (column < img[0].length - 1) {
+                setValue(img, row, column, 0, len, img[row][column]);
+                column++;
+            }
+            row++;
+            len -= 2;
+        }
+    }
+
+    private void setValue(int[][] img, int x, int y, int times, int len, int nextVal) {
+        if (times == 4) {
+            return;
+        }
+        int x1 = y;
+        int y1 = len - x;
+        int oldVal = nextVal;
+        nextVal = img[x1][y1];
+        img[x1][y1] = oldVal;
+        setValue(img, x1, y1, times + 1, len, nextVal);
+    }
+}
