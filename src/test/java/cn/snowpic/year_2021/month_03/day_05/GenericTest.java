7c166b4f1af1532df1e68a22bc9fea5919d9d74d
Little Flower <snowpic@163.com>
2021-03-06 18:26:06 +0800

    great for stones from other hill of rotating image algorithm...

--- src/test/java/cn/snowpic/year_2021/month_03/day_05/GenericTest.java
+++ src/test/java/cn/snowpic/year_2021/month_03/day_05/GenericTest.java
@@ -41,4 +41,34 @@ public class GenericTest {
         img[x1][y1] = oldVal;
         setValue(img, x1, y1, times + 1, len, nextVal);
     }
+
+    @Test
+    public void test2() {
+        int[][] img = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        rotateImg(img);
+        System.out.println(Arrays.deepToString(img));
+        int[][] img2 = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+        rotateImg(img2);
+        System.out.println(Arrays.deepToString(img2));
+    }
+
+    private void rotateImg(int[][] img) {
+        for (int i = 0; i < img.length; i++) {
+            for (int j = i + 1; j < img.length; j++) {
+                int temp = img[i][j];
+                img[i][j] = img[j][i];
+                img[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < img.length; i++) {
+            int left = 0, right = img[i].length - 1;
+            while (left < right) {
+                int temp = img[i][left];
+                img[i][left] = img[i][right];
+                img[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
 }
